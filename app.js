70ef552d4582d5c0187a0914aea5dd36f11a09f2
reneegaadee <reneegaade@hotmail.com>
2018-06-18 16:44:13 -0600

    Human years in dog years

---
+++ app.js
@@ -0,0 +1,17 @@
+const myAge = 41;
+//mi edad
+let earlyYears = 2;
+//va a cambiar
+earlyYears *= 10.5;
+let laterYears = myAge -2;
+//mi edad menos 2
+laterYears = myAge * 4;
+//para que se multiplique por 4
+console.log(`This is equivalent to the first 2 years of a dog: ${earlyYears}`);
+console.log(`This is equivalent to the remaining ${myAge -2} of a dog: ${laterYears}`);
+let myAgeInDogYears = earlyYears + laterYears;
+console.log(`My Age in dog years is: ${myAgeInDogYears}`);
+//el total de mi edad en perro
+let myName = 'Renee'.toLowerCase();
+//mi nombre en minusculas
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years`);
